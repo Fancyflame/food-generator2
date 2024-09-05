ce8526b82c9be49f58de78b63aa659b1ab38ef1e
fancyflame <fancyflame@163.com>
2024-09-05 14:33:20 +0800

    修复LcgU8在碰到排除数重新随机后仍可能为排除数的bug

--- core/src/encoder/lcg.rs
+++ core/src/encoder/lcg.rs
@@ -1,8 +1,6 @@
 const A: u16 = 110; // 选一个适合u8的乘数
 const C: u16 = 13; // 选一个增量
 const M: u16 = 256; // 模数为256，因为我们想要u8范围的输出
-const W: u8 = 17; // 当出现和初始值相同时添加的扰动
-
 pub(crate) struct LcgU8 {
     state: u8,
     excluded: u8,
@@ -17,13 +15,17 @@ impl LcgU8 {
     }
 
     pub fn next(&mut self) -> u8 {
-        let next_state = (A * self.state as u16 + C) % M;
-        self.state = next_state as u8;
+        let next_state = loop {
+            let next_state = ((A * self.state as u16 + C) % M) as u8;
+            if next_state == self.excluded {
+                self.state = self.state.wrapping_add(1);
+                continue;
+            }
 
-        if self.state == self.excluded {
-            self.state = self.state.wrapping_add(W);
-        }
+            break next_state;
+        };
 
+        self.state = next_state;
         self.state
     }
 }
