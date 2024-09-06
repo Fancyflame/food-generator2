2521d4c407c9f2d949049ae4814e29777372780d
fancyflame <fancyflame@163.com>
2024-09-06 15:49:00 +0800

    添加了一键复制和一键粘贴

--- web/fg2-app/src/index.js
+++ web/fg2-app/src/index.js
@@ -4,10 +4,9 @@ load_lib("./cache.fg2").then(() => {
     console.log("库加载完毕");
 });
 
-const inputArea = document.getElementById("input");
-const outputArea = document.getElementById("output");
-const encBtn = document.getElementById("enc-btn");
-const decBtn = document.getElementById("dec-btn");
+const [inputArea, outputArea, encBtn, decBtn, pasteBtn, copyBtn] = [
+    "input", "output", "enc-btn", "dec-btn", "paste", "copy"
+].map(id => document.getElementById(id));
 
 encBtn.onclick = () => {
     let out = GLOBAL_LIB.encode(inputArea.value);
@@ -24,3 +23,22 @@ decBtn.onclick = () => {
         outputArea.style.color = "orangered";
     }
 };
+
+pasteBtn.onclick = async function () {
+    let txt = await navigator.clipboard.readText();
+    inputArea.value = txt;
+}
+
+copyBtn.onclick = async function () {
+    let txt = outputArea.value;
+    await navigator.clipboard.writeText(txt);
+    copyBtn.innerText = "复制成功";
+    await waitTimeout(500);
+    copyBtn.innerText = "复制";
+}
+
+function waitTimeout(ms) {
+    return new Promise((resolve, _) => {
+        setTimeout(resolve, ms);
+    });
+}
